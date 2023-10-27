a7b4ad5e722afb23bc3a4f9d79fd049b5401a5b6
Antonin Pasquier <antonin.pasquier45@gmail.com>
2023-10-27 11:08:47 +0200

    creation table et route backend

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,23 +1,16 @@
 const express = require("express");
 
 const router = express.Router();
+const client = require("../database/client");
 
-/* ************************************************************************* */
-// Define Your API Routes Here
-/* ************************************************************************* */
-
-// Import itemControllers module for handling item-related operations
-const itemControllers = require("./controllers/itemControllers");
-
-// Route to get a list of items
-router.get("/items", itemControllers.browse);
-
-// Route to get a specific item by ID
-router.get("/items/:id", itemControllers.read);
-
-// Route to add a new item
-router.post("/items", itemControllers.add);
-
-/* ************************************************************************* */
+router.get("/vegetables", (req, res) => {
+  client
+    .query("SELECT * FROM vegetable")
+    .then((result) => res.status(200).json(result[0]))
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
