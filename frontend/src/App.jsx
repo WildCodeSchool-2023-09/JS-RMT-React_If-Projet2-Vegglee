95aa0f45a2679d896e2a288b8e47ee02da504e3c
vinz-DY <vincentducamp259@hotmail.com>
2023-11-07 11:29:47 +0100

    ajout de la fonction getLegumes et recuperation des datas ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,51 +1,20 @@
 import axios from "axios";
-import Counter from "./components/Counter";
-import logo from "./assets/logo.svg";
-
 import "./App.css";
 
 function App() {
-  const load = () => {
+  const getLegumes = () => {
     axios
-      .get("http://localhost:3310/api/items")
-      .then((res) => console.info(res.data))
-      .catch((error) => console.error(error));
+      .get("http://localhost:3310/api/vegetables")
+      .then((res) => res.data)
+      .catch((err) => console.error(err));
   };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React !</p>
-
-        <Counter />
-        <button onClick={() => load()} type="button">
-          load
-        </button>
-
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <h1>Mes bios légumes</h1>
+      <button type="button" onClick={getLegumes}>
+        Affiche les légumes
+      </button>
     </div>
   );
 }
