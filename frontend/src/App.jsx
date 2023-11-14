d437c04ef0caa8925bebdc84268e4e95dfc9eb97
Antonin Pasquier <antonin.pasquier45@gmail.com>
2023-11-14 12:29:30 +0100

    rajout des docs de base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,10 @@
-import { useLoaderData } from "react-router-dom";
-import LegumeCard from "./components/LegumeCard";
+import HomePage from "./pages/HomePage";
 import "./App.css";
 
 function App() {
-  const legumes = useLoaderData();
-
   return (
     <div>
-      <h1>Mes bios légumes</h1>
-      <div className="ctnCard">
-        {legumes.map((legume) => {
-          return <LegumeCard key={legume.id} legume={legume} />;
-        })}
-      </div>
+      <HomePage />
     </div>
   );
 }
---
+++ frontend/src/pages/OneVegetable.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import "./OneVegetable.css";
+
+function VegetableDetail() {
+  const { id } = useParams();
+  const [vegetable, setVegetable] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/api/vegetables/${id}`)
+      .then((response) => {
+        const vegetableData = response.data;
+        setVegetable(vegetableData);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [id]);
+
+  if (!vegetable) {
+    return null;
+  }
+
+  return (
+    <div>
+      {vegetable && (
+        <>
+          <img src={vegetable.image} alt={vegetable.name} className="test" />
+          <div className="rectangle" />
+          <div className="separate" />
+          <h2 className="name">{vegetable.name}</h2>
+          <h2 className="quantity">Quantité: {vegetable.quantityAvailable}</h2>
+          <p className="price">Prix: {vegetable.price}0 €</p>
+          <div className="centered-image">
+            <img
+              src={vegetable.image}
+              alt={vegetable.name}
+              className="imagebas"
+            />
+          </div>
+          <p className="description">{vegetable.description}</p>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default VegetableDetail;
