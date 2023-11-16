031215e1aab93406af03a28e968e2808bc1c1234
ADEL KARA <karabesch@gmail.com>
2023-11-16 15:00:20 +0100

    finition CSS de DetailsPage.css

--- frontend/src/components/Panier.jsx
+++ frontend/src/components/Panier.jsx
@@ -5,19 +5,23 @@ import "./Paniers.css";
 function Panier({ type }) {
   let imageSrc = "";
   let titre = "";
+  let prix = "";
 
   if (type === "solo") {
     imageSrc =
       "https://cdn.bioalaune.com/img/article/thumb/900x500/28986-10_fruits_et_legumes_a_deguster_en_juillet.webp";
     titre = "Panier solo";
+    prix = "20€";
   } else if (type === "duo") {
     imageSrc =
       "https://www.fermes-imagine.fr/wp-content/uploads/2019/04/panier-frais-saison-ferme-lyon.jpg";
     titre = "Panier duo";
+    prix = "35€";
   } else if (type === "family") {
     imageSrc =
       "https://cdn.bioalaune.com/img/article/thumb/900x500/33271-le_guide_des_paniers_bio_et_locaux_par_region.webp";
     titre = "Panier familial";
+    prix = "48€";
   }
 
   return (
@@ -25,7 +29,7 @@ function Panier({ type }) {
       <img src={imageSrc} alt={`panier de légumes ${type}`} />
       <div>
         <h2>{titre}</h2>
-        <p>Prix du panier</p>
+        <p>{prix}</p>
         <Link to={`/nos-paniers/${type}`}>Voir le panier</Link>
       </div>
     </div>
--- frontend/src/pages/PanierDetailsPage.jsx
+++ frontend/src/pages/PanierDetailsPage.jsx
@@ -5,13 +5,33 @@ import "./PanierDetailsPage.css";
 
 function PanierDetailsPage() {
   const [panierData, setPanierData] = useState([]);
-  const { type } = useParams();
+  const { type: panierType } = useParams();
+
+  const getPanierInfo = (type) => {
+    const info = { imageUrl: "", prix: "" };
+
+    if (type === "solo") {
+      info.imageUrl =
+        "https://cdn.bioalaune.com/img/article/thumb/900x500/28986-10_fruits_et_legumes_a_deguster_en_juillet.webp";
+      info.prix = "20€";
+    } else if (type === "duo") {
+      info.imageUrl =
+        "https://www.fermes-imagine.fr/wp-content/uploads/2019/04/panier-frais-saison-ferme-lyon.jpg";
+      info.prix = "35€";
+    } else if (type === "family") {
+      info.imageUrl =
+        "https://cdn.bioalaune.com/img/article/thumb/900x500/33271-le_guide_des_paniers_bio_et_locaux_par_region.webp";
+      info.prix = "48€";
+    }
+
+    return info;
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          `${import.meta.env.VITE_BACKEND_URL}/api/${type}_basket`
+          `${import.meta.env.VITE_BACKEND_URL}/api/${panierType}_basket`
         );
         setPanierData(response.data);
       } catch (error) {
@@ -20,18 +40,32 @@ function PanierDetailsPage() {
     };
 
     fetchData();
-  }, [type]);
+  }, [panierType]);
 
   const displayPanierData = () => {
+    const panierInfo = getPanierInfo(panierType);
     return (
-      <div className="table">
-        <h1>Panier {type}</h1>
-        {panierData.map((item) => (
-          <div key={item.id}>
-            <p>{item.product_name}</p>
-            <p>{item.quantity_or_weight}</p>
+      <div className="ContainerPanier">
+        <div className="TitlePanier">
+          <h1>Panier {panierType}</h1>
+          <h2>{panierInfo.prix}</h2>
+        </div>
+        <div className="DescriptionPanier">
+          <div className="table">
+            {panierData.map((item) => (
+              <div key={item.id}>
+                <p className="nameProduct">{item.product_name}</p>
+                <p>{item.quantity_or_weight}</p>
+              </div>
+            ))}
           </div>
-        ))}
+          <div className="ImagePanier">
+            <img
+              src={panierInfo.imageUrl}
+              alt={`panier de légumes ${panierType}`}
+            />
+          </div>
+        </div>
       </div>
     );
   };
