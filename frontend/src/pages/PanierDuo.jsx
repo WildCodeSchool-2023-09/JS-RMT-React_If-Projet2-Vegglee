af33a92f1e6e91e310e9eeb412e6ed848f65c37d
ADEL KARA <karabesch@gmail.com>
2023-11-16 15:01:52 +0100

    supression des pages solo/duo/familial qui ne sont pas utilisées

--- frontend/src/pages/PanierDuo.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function PanierDuo() {
-  return <div>PanierDuo</div>;
-}
-
-export default PanierDuo;
--- frontend/src/pages/PanierFamilial.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function PanierFamilial() {
-  return <div>PanierFamilial</div>;
-}
-
-export default PanierFamilial;
