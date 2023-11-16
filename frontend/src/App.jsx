9947f9be3de4eba8697c15fbc2ebcf40af3d7bc7
ADEL KARA <karabesch@gmail.com>
2023-11-16 21:36:14 +0100

    création du composant navbar et l'import app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import HomePage from "./pages/HomePage";
+import NavBar from "./components/NavBar";
 import "./App.css";
 
 function App() {
   return (
     <div>
+      <NavBar />
       <HomePage />
     </div>
   );
