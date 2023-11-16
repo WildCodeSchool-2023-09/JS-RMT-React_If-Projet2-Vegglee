5e3d7e58618175bebc14a682c23a8f4dbc35b614
ADEL KARA <karabesch@gmail.com>
2023-11-16 21:46:28 +0100

    création des link pour homePage et NosPaniers

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/logo.vegglee.jpg";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div>
+      <img src={Logo} alt="logo" />
+
+      <div className="NavLink">
+        <Link to="/">Home</Link>
+        <Link to="/nos-paniers">Nos Paniers</Link>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
