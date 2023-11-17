2b90c7b6d5d2972b96f0c7405fbe50f07aac853f
Julien Richard [RMT - JS] Wild Code School <33219171+jujuck@users.noreply.github.com>
2023-11-17 16:40:44 +0100

    Update main.jsx, Je vous sauve....

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import axios from "axios";
+
 import App from "./App";
 import VegetableDetail from "./pages/OneVegetable";
 import RecipePage from "./pages/RecipePage";
