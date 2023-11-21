785f418709053904995c272c316de1db65af7541
Julien Richard [RMT - JS] Wild Code School <33219171+jujuck@users.noreply.github.com>
2023-11-21 12:34:07 +0100

    Update NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
-import Logo from "../assets/favicon.jpg";
+import Logo from "../assets/favicon.svg";
 
 function NavBar() {
   return (
